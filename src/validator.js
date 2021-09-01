4fe76b85e509d5e228e4a4b882af80c0cf517d90
yusjuani <veroyusjuani@gmail.com>
2021-09-01 08:37:20 -0400

    primer sprint

--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,42 @@ const validator = {
 };
 
 export default validator;
+
+
+function ordenar(){
+  let array= [];
+  for(let i = 0; i < 10 ; i++);{ 
+      array.reverse();
+   }
+   
+  let number= document.getElementById("number").value;
+  console.log(number);
+  //function reversa()
+  
+}
+
+//function reversa(){
+ //array= number.reverse();
+ //conloge.log(number);
+  
+  
+  
+//}
+
+/*function card(){
+  let number= document.getElementById("number").value;
+  console.log(number);
+  
+}*/
+
+//let number = [1, 2, 3, ];
+
+//for(let i = number.length -1; i >= 0; i--);{ 
+  //console.log(number[i]);
+//}
+
+
+//console.log();
+//for(const n of number.reverse()){
+   //console.log(n);
+//}
