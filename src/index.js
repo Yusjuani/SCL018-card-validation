301cc22c9d992be9fab70c7f8db8d9fec1f19241
yusjuani <veroyusjuani@gmail.com>
2021-09-08 19:32:34 -0300

    proyecto

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import validator from './validator.js';
 let validar = document.getElementById("validar");//presionar botton
 validar.addEventListener("click", () =>{        // al escuchar click generar funcion
 let inputValue= document.getElementById("number").value; //se creo variable, que tomara valor input
-
+if(validator.isValid(inputValue)  === false){       //si esta funcion es %10
+document.getElementById("regalo").style.display="block"; //aparezca regalo
+    
+}
 validator.isValid(inputValue) // llame funcion de isValid  de validator.js
+console.log(validator.isValid(inputValue));
 
+//aparezca regalo
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,13 +1,51 @@
 const validator = {
     isValid(inputValue){ //funcion que toma valor input
         let arrayNumber = inputValue.split(""); //convertir string en array
-        console.log(arrayNumber);
+        //console.log(arrayNumber);
         let reverseNumber= arrayNumber.reverse();// convertir el array en reverso
-        console.log(reverseNumber);
-        return reverseNumber; //retornar el resultado
-        
+        //console.log(reverseNumber);
+        //return reverseNumber; //retornar el resultado
+      
+       let addNumber = 0 //variable que
+       for(let i = 0; i < reverseNumber.length; i++){
+           const number = reverseNumber[i];
+           const position = i + 1;
 
-}}
+           if(position % 2 === 0){
+               const numberMultiplicado = number * 2;
+            if(numberMultiplicado >= 10){
+                const numeroSeparado = numberMultiplicado.toString().split("");
+                const firtNumber = parseInt(numeroSeparado[0]);
+                const secondNumber = parseInt(numeroSeparado[1]);
+                const amountNumber = firtNumber + secondNumber;
+                addNumber += amountNumber;
+
+            }   else{
+                addNumber += numberMultiplicado;
+            }        
+        }else{ 
+            addNumber += parseInt(number);
+
+            }
+       }
+   console.log(addNumber);
+   console.log(reverseNumber);
+
+   if(addNumber % 10 === 0){
+       return true;
+   }else{
+       return false;
+   }
+   
+   function maskify(cc) {
+    for(let i = 0; i < cc.length; i++){
+        
+    
+   
+  
+}}}
+
+
 
 
 
