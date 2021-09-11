253e14480ca495ac5fe5b42e55b6f1a77f2c29e4
yusjuani <veroyusjuani@gmail.com>
2021-09-11 13:38:14 -0300

    firt commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import validator from './validator.js';
 let validar = document.getElementById("validar");//presionar botton
 validar.addEventListener("click", () =>{        // al escuchar click generar funcion
 let inputValue= document.getElementById("number").value; //se creo variable, que tomara valor input
-if(validator.isValid(inputValue)  === false){       //si esta funcion es %10
-document.getElementById("regalo").style.display="block"; //aparezca regalo
-    
+if(validator.isValid(inputValue)  === true){       //si esta funcion es %10
+document.getElementById("regalo").style.display="block"; //aparezca regalo   
+alert("Tarjeta valida");
+}else{
+    alert("card no valid");
 }
+document.getElementById("number").value = ""; //desaparezca el valor input
+
 validator.isValid(inputValue) // llame funcion de isValid  de validator.js
 console.log(validator.isValid(inputValue));
-
-//aparezca regalo
-
-
+console.log(validator.maskify(inputValue));//llamo funcion maskify(tomo ojeto con funcion mas valor)
+let mostrarDigitos = document.getElementById("mostrarDigitos");//tomar id de parrafo
+mostrarDigitos.innerHTML = "El número de su tarjeta es : " + validator.maskify(inputValue) ;//mostrar js en html
 
 });
- 
+
--- src/validator.js
+++ src/validator.js
@@ -7,17 +7,17 @@ const validator = {
         //return reverseNumber; //retornar el resultado
       
        let addNumber = 0 //variable que
-       for(let i = 0; i < reverseNumber.length; i++){
+       for(let i = 0; i < reverseNumber.length; i++){//recorrer los numeros
            const number = reverseNumber[i];
-           const position = i + 1;
+           const position = i + 1; //tomar numeros pares
 
-           if(position % 2 === 0){
-               const numberMultiplicado = number * 2;
-            if(numberMultiplicado >= 10){
-                const numeroSeparado = numberMultiplicado.toString().split("");
-                const firtNumber = parseInt(numeroSeparado[0]);
+           if(position % 2 === 0){  //numero par
+               const numberMultiplicado = number * 2;//multiplicarlo por 2
+            if(numberMultiplicado >= 10){ // si es mayor o igual a 10
+                const numeroSeparado = numberMultiplicado.toString().split(""); //pasarlo a string y despues convertirlo a array
+                const firtNumber = parseInt(numeroSeparado[0]);//pasarlos a numeros
                 const secondNumber = parseInt(numeroSeparado[1]);
-                const amountNumber = firtNumber + secondNumber;
+                const amountNumber = firtNumber + secondNumber;// sumarlos 
                 addNumber += amountNumber;
 
             }   else{
@@ -36,20 +36,29 @@ const validator = {
    }else{
        return false;
    }
-   
-   function maskify(cc) {
-    for(let i = 0; i < cc.length; i++){
-        
+},
+  //maskify
+
+     maskify(inputValue){
+     const numeroInvertido = inputValue.split("").reverse(); //tomo el input original y convierto en array lo reverso
+     console.log(numeroInvertido);
+
+     let inputTarjeta = "";
+     for(let i = 0; i < numeroInvertido.length; i++){
+         if(i < 4){
+            inputTarjeta += numeroInvertido[i]
+             
+         }else{
+             inputTarjeta += "#"
+         }
+         
+     }
+     const numberMaskify = inputTarjeta.split("").reverse().join("");
+     return numberMaskify;
+    }
+     
+}        
     
-   
-  
-}}}
-
-
-
-
-
-
 
 
 export default validator;
