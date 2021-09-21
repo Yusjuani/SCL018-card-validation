f70fbe57d0efe12fa69ddcc6f2af35a9b36003ba
yusjuani <veroyusjuani@gmail.com>
2021-09-21 09:49:04 -0300

    identacion

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
+let validar = document.getElementById("validar"); //presionar botton
+validar.addEventListener("click", () => {
+  // al escuchar click generar funcion
 
-    
-
-let validar = document.getElementById("validar");//presionar botton
-validar.addEventListener("click", () =>{        // al escuchar click generar funcion
-
-let inputValue= document.getElementById("number").value; //se creo variable, que tomara valor input
-if(validator.isValid(inputValue)  === true){       //si esta funcion es %10
-document.getElementById("regalo").style.display="block"; //aparezca regalo   
-alert("Tarjeta valida");
-}else{
+  let inputValue = document.getElementById("number").value; //se creo variable, que tomara valor input
+  if (validator.isValid(inputValue) === true) {
+    //si esta funcion es %10
+    document.getElementById("regalo").style.display = "block"; //aparezca regalo
+    alert("Tarjeta valida");
+  } else {
     alert("Ingresa un número válido");
-}
+  }
 
-document.getElementById("number").value = ""; //desaparezca el valor input
+  document.getElementById("number").value = ""; //desaparezca el valor input
 
-
-//console.log(validator.isValid(inputValue));
-//console.log(validator.maskify(inputValue));//llamo funcion maskify(tomo ojeto con funcion mas valor)
-let mostrarDigitos = document.getElementById("mostrarDigitos");//tomar id de parrafo
-mostrarDigitos.innerHTML = "El número de su tarjeta es : " + validator.maskify(inputValue) ;//mostrar js en html
-
+  //console.log(validator.isValid(inputValue));
+  //console.log(validator.maskify(inputValue));//llamo funcion maskify(tomo ojeto con funcion mas valor)
+  let mostrarDigitos = document.getElementById("mostrarDigitos"); //tomar id de parrafo
+  mostrarDigitos.innerHTML =
+    "El número de su tarjeta es : " + validator.maskify(inputValue); //mostrar js en html
 });
-
